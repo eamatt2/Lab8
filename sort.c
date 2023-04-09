c9f5bf77b9b9bbf8f0169474fd93b704c33aabf3
eamatt2 <125511922+eamatt2@users.noreply.github.com>
2023-04-09 19:10:52 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -61,71 +61,67 @@ for (int i = n - 1; i >= 0; i--) {
 
 }
 
-void merge(int arr[], int l, int m, int r)
+void merge(int pData[], int l, int m, int r)
 {
 int i, j, k;
-int n1 = m - l + 1;
-int n2 = r - m;
-/* create temp arrays */
-int *L = (int*) malloc(n1*sizeof(int));
-int *R = (int*) malloc(n2*sizeof(int));
-/* Copy data to temp arrays L[] and R[] */
-for (i = 0; i < n1; i++)
-	L[i] = arr[l + i];
-for (j = 0; j < n2; j++)
-	R[j] = arr[m + 1+ j];
-/* Merge the temp arrays back into arr[l..r]*/
-i = 0; // Initial index of first subarray
-j = 0; // Initial index of second subarray
-k = l; // Initial index of merged subarray
-while (i < n1 && j < n2)
-{
-	if (L[i] <= R[j])
-	{
-		arr[k] = L[i];
-		i++;
-	}
-	else
-	{
-		arr[k] = R[j];
-		j++;
-	}
-	k++;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+  //create space for the 2 arrays
+    int* L = (int*)malloc(n1 * sizeof(int));
+    int* R = (int*)malloc(n2 * sizeof(int));
+    extraMemoryAllocated += n1 * sizeof(int) + n2 * sizeof(int);
+    for (i = 0; i < n1; i++) {
+        L[i] = pData[l + i];
+    }
+    for (j = 0; j < n2; j++) {
+        R[j] = pData[m + 1 + j];
+    }
+    i = 0;
+    j = 0;
+    k = l;
+  //iterates through left and right arrays
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            pData[k] = L[i];
+            i++;
+        }
+        else {
+            pData[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+  //copies elements from left into new merged array
+    while (i < n1) {
+        pData[k] = L[i];
+        i++;
+        k++;
+    }
+  //copies elements from right into new merged array
+    while (j < n2) {
+        pData[k] = R[j];
+        j++;
+        k++;
+    }
+    free(L);
+    free(R);
 }
-/* Copy the remaining elements of L[], if there
-are any */
-while (i < n1)
-{
-	arr[k] = L[i];
-	i++;
-	k++;
-}
-/* Copy the remaining elements of R[], if there
-are any */
-while (j < n2)
-{
-	arr[k] = R[j];
-	j++;
-	k++;
-}
-free(L);
-free(R);
+
+void mergeSortHelper(int pData[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSortHelper(pData, l, m);
+        mergeSortHelper(pData, m + 1, r);
+        merge(pData, l, m, r);
+    }
 }
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	if (l < r)
-{
-// get the mid point
-	int m = (l+r)/2;
-// Sort first and second halves
-	mergeSort(pData, l, m);
-	mergeSort(pData, m+1, r);
-// printf("Testing l=%d r=%d m=%d\n", l, r, m);
-	merge(pData, l, m, r);
-}
+	extraMemoryAllocated = 0;
+    mergeSortHelper(pData, l, r);
 }
 
 // parses input file to an integer array
